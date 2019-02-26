687841658138480dc134ec00e8addfd01f734b7a
louieKay <37403561+louieKay@users.noreply.github.com>
2019-02-26 21:36:16 +0000

    add expenses from same win, search, filter, save, load

--- Labsheet3/MainWindow.xaml.cs
+++ Labsheet3/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Windows;
+using Newtonsoft.Json;
 
 namespace Labsheet3
 {
@@ -12,38 +14,54 @@ namespace Labsheet3
     {
         ObservableCollection<Expenses> expenses = new ObservableCollection<Expenses>();
 
-        public MainWindow()
+        ObservableCollection<Expenses> matchingExpenses = new ObservableCollection<Expenses>();
+
+        string[] categories = {"travel", "office", "entertainment" };
+
+    public MainWindow()
         {
             InitializeComponent();
         }
         
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            cbxFilter.ItemsSource = categories;
+            cbxSelectCategory.ItemsSource = categories;
         }
         
 
-        private void BtnRemove_Click(object sender, RoutedEventArgs e)
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
         {
             expenses.RemoveAt(expenses.Count()-1);
             txbResult.Text = GetTotalCost().ToString();
             txbNumber.Text = expenses.Count().ToString();
-
         }
 
         private void btnAddExpenses_Click(object sender, RoutedEventArgs e)
         {
-            Expenses ex = new Expenses();
-            ex.Category = tbxCategory.Text;
-            ex.Price = Convert.ToDecimal(tbxCost.Text);
-            ex.ExpenseDate = dpExpenseDate.SelectedDate.Value;
+            //Expenses ex = new Expenses();
+            //if (!String.IsNullOrEmpty(tbxCategory.Text))
+            //{
+            //    ex.Category = tbxCategory.Text;
+            //}
+            //else
+            //{
+            //    ex.Category = cbxSelectCategory.SelectedItem as string;
+            //}
 
-            expenses.Add(ex);
+            //ex.Price = Convert.ToDecimal(tbxCost.Text);
+            //ex.ExpenseDate = dpExpenseDate.SelectedDate.Value;
 
-      
-            lbxExpenses.ItemsSource = expenses;
-            txbResult.Text = GetTotalCost().ToString();
-            txbNumber.Text = expenses.Count().ToString();
+            //expenses.Add(ex);
+
+
+            //lbxExpenses.ItemsSource = expenses;
+            //txbResult.Text = GetTotalCost().ToString();
+            //txbNumber.Text = expenses.Count().ToString();
+
+
+            AddExpense addExp = new AddExpense();
+            addExp.ShowDialog();
 
         }
 
@@ -57,6 +75,77 @@ namespace Labsheet3
             return total;
         }
 
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = tbxSearch.Text;
 
+            if (!String.IsNullOrEmpty(searchTerm))
+            {
+                matchingExpenses.Clear();
+                foreach(Expenses exp in expenses)
+                {
+                    string expenseType = exp.Category;
+                    if (expenseType.Equals(searchTerm))
+                    {
+                        matchingExpenses.Add(exp);
+                    }
+                }
+            }
+
+            lbxExpenses.ItemsSource = matchingExpenses;
+
+        }
+
+        private void BtnShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            lbxExpenses.ItemsSource = expenses;
+        }
+
+        private void CbxFilter_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            string selectedType = cbxFilter.SelectedItem as string;
+
+            if(selectedType != null)
+            {
+                matchingExpenses.Clear();
+
+                foreach(Expenses exp in expenses)
+                {
+                    string expCategory = exp.Category;
+
+                    if (expCategory.Equals(selectedType))
+                    {
+                        matchingExpenses.Add(exp);
+                    }
+                }
+
+                lbxExpenses.ItemsSource = matchingExpenses;
+            }
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(expenses, Formatting.Indented);
+
+            using (StreamWriter sw = new StreamWriter(@"e:\temp\expenses.json"))
+            {
+                sw.Write(json);
+            }
+
+            MessageBox.Show("values saved");
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(@"e:\temp\expenses.json"))
+            {
+                string json = sr.ReadToEnd();
+                expenses = JsonConvert.DeserializeObject<ObservableCollection<Expenses>>(json);
+
+                lbxExpenses.ItemsSource = expenses;
+            }
+
+            MessageBox.Show("values loaded");
+        }
     }
 }
